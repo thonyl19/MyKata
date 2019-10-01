5562499d7241311c6f95970825baabc4756196d0
thony <a0982830615@gmail.com>
2019-10-01 16:40:51 +0800

    Add>SimpleFactory

--- CSharp/UnitTest1.cs
+++ CSharp/UnitTest1.cs
@@ -1,3 +1,5 @@
+using CSharp.DesingPattern;
+using CSharp_2019;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CSharp
@@ -5,10 +7,21 @@ namespace CSharp
     [TestClass]
     public class UnitTest1
     {
+        // [TestMethod]
+        // public void TestMethod1()
+        // {
+        //     var r = A03.Test(519);
+        // }
+
         [TestMethod]
-        public void TestMethod1()
+        public void SimpleFactory()
         {
-            A01.Test();
+            var store = new SimpleFactory();
+            var total = store.NewOrder()
+                .Order(eDrinks.GreenTea,2)
+                .Order(eDrinks.Coffee,2)
+                .Checkout();
+            Assert.AreEqual(total,70);
         }
     }
 }
