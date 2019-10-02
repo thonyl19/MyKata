b728aaf06833d0af2d9dd153f7b612491c78c148
thony <a0982830615@gmail.com>
2019-10-02 21:10:35 +0800

    Add>SPEC memo

--- CSharp/Codility/L01.cs
+++ CSharp/Codility/L01.cs
@@ -1,3 +1,11 @@
+/*
+SPEC>
+    1.將整數轉成 二進位值
+    2.計算二進位值中,連續零(間隔)最長長度
+    3.連續零結尾的下一位數如果不為1,則不列計該連續零長度
+    4.如果不包含間隔則函數應返回 0.
+*/
+
 using System;
 
 namespace CSharp_Codility
@@ -5,10 +13,20 @@ namespace CSharp_Codility
     public class L01
     {
         public static int Test(int val){
-            return BinaryGap(val);
+            return fn(val);
         }
 
-        static int BinaryGap(int N){
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="N"></param>
+        /// <returns></returns>
+        /// SPEC>
+        /// 1.將整數轉成 二進位值
+        /// 2.計算二進位值中,連續零(間隔)最長長度
+        /// 3.連續零結尾的下一位數如果不為1,則不列計該連續零長度
+        /// 4.如果不包含間隔則函數應返回 0.
+        static int fn(int N){
             string x = Convert.ToString(N,2);
             string[] arr = x.Split("1");
             int maxGap = 0;
