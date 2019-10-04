813b8a3b55a6cdc64c3c4b1ee982dc6d06c0b48d
thony <a0982830615@gmail.com>
2019-10-04 12:24:05 +0800

    Add>L022-CyclicRotation

--- CSharp/UnitTest_Codility.cs
+++ CSharp/UnitTest_Codility.cs
@@ -20,14 +20,14 @@ namespace CSharp
         [TestMethod]
         public void t_L021()
         {
+            Func<int[] ,bool,dynamic> fn =  L021.solution_0_linq;
             for (var i=0 ; i < 100 ;i++){
-                var r = 
-                    L021.GenTest(100003,false,100003);
+                var r =  L021.GenTest(100003,false,100003);
                     // new {
                     //     one =1 ,
                     //     list =  new int[]{1,7,7,7,9,9,9}
                     // };
-                var t1_A = UT.Timer(()=>{return  L021.solution_0_linq(r.list,true);});
+                var t1_A = UT.Timer(()=>{return  fn(r.list,true);});
                 Assert.AreEqual(t1_A.Ans,r.one);
             }
         }
@@ -38,6 +38,19 @@ namespace CSharp
             int _range = 11 ;
             var r = L021.GenTest(_range);
             Assert.AreEqual(r.list.Length,_range);
+        }
+
+
+        [TestMethod]
+        public void t_L022()
+        {
+            Func<int[],int,int[]> fn = L022.solution_0;
+            int[] arr =  new int[]{1,2,3,4,5,6};
+            Assert.AreEqual("123456",string.Join("",fn(arr,6)));
+            Assert.AreEqual("612345",string.Join("",fn(arr,7)));
+            Assert.AreEqual("561234",string.Join("",fn(arr,2)));
+            Assert.AreEqual("345612",string.Join("",fn(arr,10)));
+            Assert.AreEqual("234561",string.Join("",fn(arr,11)));
         }
 
         
