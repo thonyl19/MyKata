d8d69af3377ba379b580c07980e848b05ff9e1c9
anthony <a0982830615@gmail.com>
2020-01-13 17:12:02 +0800

    Add>處理個案需求

--- Web/PowerTool/App_Str.js
+++ Web/PowerTool/App_Str.js
@@ -12,7 +12,7 @@
 }(this, function ($) {
     String.prototype.format = function (args) {
         var str = this;
-        return str.replace(String.prototype.format.regex, function(item) {
+        return str.replace(String.prototype.format.regex, function (item) {
             var intVal = parseInt(item.substring(1, item.length - 1));
             var replace;
             if (intVal >= 0) {
@@ -30,85 +30,88 @@
     String.prototype.format.regex = new RegExp("{-?[0-9]+}", "g");
 
     let Txt_App = {
-        Replace(Target,Old,New){
-            var reg = new RegExp(Old,'gi');
+        Replace(Target, Old, New) {
+            var reg = new RegExp(Old, 'gi');
             return Target.replace(reg, New);
         },
-        條件換行(Target,SplitWord,isKeepSplitWord=false){
+        條件換行(Target, SplitWord, isKeepSplitWord = false) {
             let arr = Target.split(SplitWord);
-            let _join = `${isKeepSplitWord?SplitWord:""}\n`
+            let _join = `${isKeepSplitWord ? SplitWord : ""}\n`
             return arr.join(_join);
         },
-        清前後空白(Target,isAutoJoin=true){
+        清前後空白(Target, isAutoJoin = true) {
             let arr = Target.split('\n');
             let r = []
             arr.forEach(el => {
                 r.push(el.trim());
             });
-            if (isAutoJoin){
-                return r.join('\n') ;
+            if (isAutoJoin) {
+                return r.join('\n');
             }
             return r;
         },
-        SplitByLine(Target,AutoTrim=true){
-            let arr = AutoTrim 
-                ? Txt_App.清前後空白(Target,false)
+        SplitByLine(Target, AutoTrim = true) {
+            let arr = AutoTrim
+                ? Txt_App.清前後空白(Target, false)
                 : Target.split('\n');
             return arr;
         },
-        字串集結(Target,AutoTrim=true,JoinSplitWord=null){
-            let arr = Txt_App.SplitByLine(Target,AutoTrim);
-            if (JoinSplitWord != null){
+        字串集結(Target, AutoTrim = true, JoinSplitWord = null) {
+            let arr = Txt_App.SplitByLine(Target, AutoTrim);
+            if (JoinSplitWord != null) {
                 return arr.join(JoinSplitWord);
             }
             return arr;
         },
-        字串補定長(Target,LineSpan,AddWord=" ",AddAfter=true,AutoTrim=true,isAutoJoin=true){
-            let arr = Txt_App.SplitByLine(Target,AutoTrim);
+        字串補定長(Target, LineSpan, AddWord = " ", AddAfter = true, AutoTrim = true, isAutoJoin = true) {
+            let arr = Txt_App.SplitByLine(Target, AutoTrim);
             let r = []
             arr.forEach(el => {
-                if (LineSpan!=null){
-                    if (AddAfter){
-                        el = el.padEnd(LineSpan,AddWord);
-                        
-                    }else{
-                        el = el.padStart(LineSpan,AddWord);
+                if (LineSpan != null) {
+                    if (AddAfter) {
+                        el = el.padEnd(LineSpan, AddWord);
+
+                    } else {
+                        el = el.padStart(LineSpan, AddWord);
                     }
-                } 
+                }
                 r.push(el);
             });
-            if (isAutoJoin){
-                return r.join('\n') ;
+            if (isAutoJoin) {
+                return r.join('\n');
             }
             return r;
         },
-        FormatTab(Target,tabs,isAutoJoin=true){
-            let _tab = _.repeat('\t',tabs);
+        FormatTab(Target, tabs, isAutoJoin = true) {
+            let _tab = _.repeat('\t', tabs);
             let r = []
             if (_.isString(Target)) Target = Target.split('\n');
-            Target.forEach((line)=>{
+            Target.forEach((line) => {
                 r.push(`${_tab}${line}`);
             });
             if (isAutoJoin) return r.join('\n');
             return r;
         },
-        _tpl_RegEx(idx)
-        {
+        _tpl_RegEx(idx) {
             return new RegExp(`({)?\\{${idx}\\}(?!})`, 'gm')
         },
-        Tpl_ByLine(Target,tpl,col_split=",",AutoTrim=true,isAutoJoin=true){
-            let arr = Txt_App.SplitByLine(Target,AutoTrim);
-            col_split = col_split.replace(new RegExp('\\','gm'))
+        Tpl_ByLine(Target, tpl, col_split = ",", AutoTrim = true, isAutoJoin = true) {
+            let arr = Txt_App.SplitByLine(Target, AutoTrim);
+            //col_split = col_split.replace(new RegExp('\\','gm'))
+            tpl =
+                `<div class="form-group col-lg-6">
+            <label>@RES.BLL.Face.{0}</label>
+            <input class="form-control" type="text" v-model="CurrentLot.{0}" readonly />
+        </div>`;
             var r = [];
-            arr.forEach((line)=>{
+            arr.forEach((line) => {
                 debugger
                 let _arr = line.split(col_split);
                 r.push(tpl.format(_arr));
             });
             if (isAutoJoin) return r.join('\n');
             return r;
-         }
+        }
     };
     return Txt_App;
 }));
- 
