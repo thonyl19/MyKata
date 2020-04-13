71c8331c5d95f292a6605c990debe42fe5c7574e
anthony <a0982830615@gmail.com>
2020-04-13 09:18:25 +0800

    Add

--- Web/QuokkaApp/Regex.js
+++ Web/QuokkaApp/Regex.js
@@ -1,5 +1,26 @@
 import * as _ from 'lodash';
+var list_number = [
+    '100.11',
+    '100',
+    '-100',
+    '-100.1',
+    '-100.11',
+    '$100.11',
+    '-$100.11',
+    '.11',
+    '10%',
+    '10.1%',
+    '-10%',
+    '-10.1%',
+];
 var fn = {
+    gen(reg,list=list_number){
+        var arr = []
+        _.each(list,(e)=>{
+           arr.push({val:e,test:e.match(reg)});
+        })
+        arr;
+    },
     '解析 %(1.5'(){
         var _reg = /(|[0-9]+%)\(([0-9]+|)(\.[0-9]+|)$/g;
         var _list = [
@@ -8,43 +29,34 @@ var fn = {
             '移植到 2019 版(1.5',
             '移植到 2019 版(1'
         ];
-        var arr = []
-        _.each(_list,(e)=>{
-           arr.push([e,e.match(_reg)]);
-        })
-        arr;
+        
     },
     '正負數,小數'(){
         var _reg = /(\-|\+)?\d+(\.\d)?$/g;
-        var _list = [
-            '100.1',
-            '100',
-            '-100',
-            '-100.1',
-        ];
-        var arr = []
-        _.each(_list,(e)=>{
-           arr.push([e,e.match(_reg)]);
-        })
-        arr;
+        fn.gen(_reg)
     },
-    '_有兩位小數'(){
+    '有兩位小數'(){
         //https://kknews.cc/code/65y4v9v.html
         //不正確
         var _reg = /^[0-9]+(.[0-9]{2})?$/g;
-        var _list = [
-            '100.1',
-            '100',
-            '-100',
-            '-100.1',
-        ];
-        var arr = []
-        _.each(_list,(e)=>{
-           arr.push([e,e.match(_reg)]);
-        })
-        arr;
+        fn.gen(_reg);
     },
+    'm-n位的數字'(){
+        var _reg = /^\d{1,3}$/g;
+        fn.gen(_reg);
+    },
+    '漢字'(){
+        //https://kknews.cc/code/m2pox22.html
+        var _reg = /^[\u4e00-\u9fa5]{0,}$/g;
 
+    },
+    'Email地址'(){
+        var _reg = /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/g;
+    },
+    '手機號碼'(){
+        //"XXX-XXXXXXX"、"XXXX-XXXXXXXX"、"XXX-XXXXXXX"、"XXX-XXXXXXXX"、"XXXXXXX"和"XXXXXXXX
+        var _reg = /^(\(\d{3,4}-)|\d{3.4}-)?\d{7,8}$/g;
+    },
 }
 
 _.each(fn,(e,k)=>{
