44ee2955803976ebf289b66847d274bd87fba33e
thony <a0982830615@gmail.com>
2020-04-16 20:19:01 +0800

    Add

---
+++ Web/Code_Sample/selectize_0.12.6.js
@@ -0,0 +1,17 @@
+﻿let Views = {
+  std1() {
+      var _obj = {
+         _vue:{
+            template: `
+            <div>
+            </div>
+            `
+         }};
+      return _obj;
+   }
+};
+
+window.sample = { 
+  Views 
+  ,def:'std1' 
+};
