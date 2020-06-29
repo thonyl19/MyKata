1014a1d0a6b5e61066edb826ef91e4d9985e0208
anthony <a0982830615@gmail.com>
2020-06-29 09:48:58 +0800

    Add

--- CSharp/Linq/UnitTest.cs
+++ CSharp/Linq/UnitTest.cs
@@ -1,8 +1,9 @@
 using System.Collections.Generic;
+using System.Linq;
 using CSharp.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace CSharp.Linq
+namespace CSharp.LinqEx
 {
     [TestClass]
     public class UnitTest
@@ -14,6 +15,22 @@ namespace CSharp.Linq
             L01.Fn2();
         }
  
+        /*
+        https://dotblogs.com.tw/willsublog/2018/05/30/104022
+        */
+        [TestMethod]
+        public void t_FilterEndWith()
+        {
+            var list = new List<string>() {"ASUS_PC","Acer_PC","BenQ", "Toshiba", "IBM", "HP", "Dell", "PChome"};
+            //var _filter = "Q";
+            var s1 = list.Where(p => p.Contains("PC")).ToList();
+            var s2 = list.Where(p => p.StartsWith("PC")).ToList();
+            var s3 = list.Where(p => p.EndsWith("PC")).ToList();
+            var s4 = list
+                .Where(p => p.EndsWith("PC") || p.EndsWith("Q"))
+                .ToList();
+             
+        }
  
     }
 }
