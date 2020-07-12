0a568354a7ed1f980694fbf7f850fa7062c08c6f
thony <a0982830615@gmail.com>
2020-07-12 23:26:59 +0800

    Add

--- Web/Code_Sample/_base.js
+++ Web/Code_Sample/_base.js
@@ -1,26 +1,26 @@
-﻿let Views = {
-   std1() {
-      var _note = `
-         <pre>
-         </pre>
-         `;
-      var _obj = {
-         _vue: {
-            template: `
-               <div>
-                  ${_note}
-               </div>
-               `,
-            data(){
-               return {
-              }
-            } 
-         }
-      };
-      return _obj;
-   },
-};
-
-window.sample = { 
-  Views 
-};
+﻿   let Views = {
+      std1() {
+         var _note = `
+            <pre>
+            </pre>
+            `;
+         var _obj = {
+            _vue: {
+               template: `
+                  <div>
+                     ${_note}
+                  </div>
+                  `,
+               data(){
+                  return {
+                 }
+               } 
+            }
+         };
+         return _obj;
+      },
+   };
+   
+   window.sample = { 
+     Views 
+   };
