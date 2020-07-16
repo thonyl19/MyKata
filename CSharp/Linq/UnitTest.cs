c6bb5544f734c94ee7b15e30c4b1e7214e51f84a
anthony <a0982830615@gmail.com>
2020-07-16 15:45:48 +0800

    Add>t_List_Dictionary

--- CSharp/Linq/UnitTest.cs
+++ CSharp/Linq/UnitTest.cs
@@ -31,6 +31,33 @@ namespace CSharp.LinqEx
                 .ToList();
              
         }
- 
+
+        /// <summary>
+        /// 工作應用的情境,需要將字串資料 轉為以下格式
+        ///     [{"A":""},{"B":""},{"C":""}]
+        /// 然後前端再用進個做回傳,由後端接收處理,
+        ///     同樣是以 List<Dictionary<string, string>> 格式做接收即可
+        /// </summary>
+        [TestMethod]
+		public void t_List_Dictionary()
+		{
+			var s = "A,B,C";
+			var data = s.Split(',').Select(x =>{
+				return new Dictionary<string, string>() { { x, "" } };
+			}).ToList();
+
+			var z = new Dictionary<string, string>() { { "A", "" } };
+
+			var _data = new List<Dictionary<string, string>>
+			{
+				new Dictionary<string, string>() { { "Cat", "" } },
+				new Dictionary<string, string>() { { "Owl", "" } },
+				new Dictionary<string, string>() { { "Rat", "" } },
+				new Dictionary<string, string>() { { "Bat", "" } },
+			};
+			//new FileApp().Write_SerializeJson(data, FileApp.ts_Log(@"t_.json"));
+
+		}
+
     }
 }
