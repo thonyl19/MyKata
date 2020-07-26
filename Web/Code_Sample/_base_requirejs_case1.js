35c41eea3126a87193c4a5476f6e55a21ddea8c4
thony <a0982830615@gmail.com>
2020-07-26 23:45:03 +0800

    add>tmp

---
+++ Web/Code_Sample/_base_requirejs_case1.js
@@ -0,0 +1,20 @@
+﻿require.config({
+  	//避免緩存
+	urlArgs: "bust=" + new Date().getTime(),
+	paths: {
+		"bts45":"https://cdn.jsdelivr.net/npm/bootstrap@4.5.0/dist/js/bootstrap.min",
+		"bts45-css":"https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min",
+	},
+	map: {
+		"*": {
+			css:"https://cdnjs.cloudflare.com/ajax/libs/require-css/0.1.10/css.min.js",
+		},
+	},
+	//依賴
+    shim: {
+        'bts45':{deps: ['css!bts45-css']},
+    }
+});
+require(["jquery","vue","bts45"], ($,Vue,bts45)=> {
+	debugger
+});
