ba15dc2277260d02eeba6d51f5de38c3d1710fb8
thony <a0982830615@gmail.com>
2020-09-19 23:38:47 +0800

    Add

--- Web/cypress/integration/gti_spec.js
+++ Web/cypress/integration/gti_spec.js
@@ -7,14 +7,41 @@ Cypress.on('window:before:load', (win) => {
 	debugger
 	delete win.fetch	
   })
+  var fn = {
+	T01(){
+		cy.visit('http://118.163.52.55/GenesisNewMes');
+		//cy.get('.navbar-nav a:eq(0)').click();
+		
+		//cy.get('a[title=登入系統]').click();
+		cy.get('#user-block-toggle').click();
+		//cy.contains('button','立即登入')
+		cy.get('.swal2-confirm').click();
+	},
+	Login(){
+		cy.visit('http://118.163.52.55/GenesisNewMes/pages/Login');
+		cy.get('#Account').type('admin'); 
+		cy.get('#Password').type('gtimes135');
+		cy.get('.btn').click();
+			//.focus();
+	},
+	T03(){
+		fn.Login();
+		cy.get(':nth-child(1) > :nth-child(3) > [data-toggle="collapse"] > span', { timeout: 10*1000 })
+		cy.visit('http://118.163.52.55/GenesisNewMes/WIP/Test01/single?name=case1');
+		cy.get('#app > button')
+			.click()
+			.click();
+		cy.get('#app > :nth-child(1)')
+			.contains('counts:2')
+		// cy.get('#app > :nth-child(1)')
+		// 	.should('have.text','counts:2');
+			//.invoke("text")
+			// .should(($el)=>{
+			// 	expect($el.text()).to.eq('counts:2');
+			// });
+	}
+}
 describe('My First Test', function() {
-	it('開啟頁面', function() {
-	  cy.visit('http://118.163.52.55/GenesisNewMes');
-	  
-	  //cy.get('.navbar-nav a:eq(0)').click();
-	  
-	 //cy.get('a[title=登入系統]').click();
-	 cy.get('#user-block-toggle').click();
-	 cy.contains('button','立即登入').click();
-	})
+	it('開啟頁面', fn.T03)
   })
+
