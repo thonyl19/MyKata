623748d70c65c3a7b5405ed117ebd2fde870b738
anthony <a0982830615@gmail.com>
2020-10-05 15:24:40 +0800

    Add>Reload-Timer

--- Web/Code_Sample/element-ui-2.13.js
+++ Web/Code_Sample/element-ui-2.13.js
@@ -281,296 +281,354 @@ var __fn = (
 			};
 			return _obj;
 		},
-	  'bts.form-control'() {
-		/*
-		在套用 form-control 之後 , element UI 的樣式會出現跑版的問題,
-		  目前仍找不到比較好的解決方案
-		*/
-		var _vue = {
-		  template: `
-					<div>
-					<div><input type="checkbox" v-model="isUse" />套用 form-control</div>
-					<el-select :class="[isUse?'form-control':'']"  v-model="sel">
-					  <el-option v-for="item in list_box_type"
-							   :key="item.value"
-							   :label="item.label"
-							   :value="item.value">
-					</el-option>
-				</el-select>
-					</div>`,
-					data(){
-					  return {
-						isUse:true,
-						sel:0,
-						list_box_type: [
-						  {
-							  value: 0,
-							  label: 'ZGB'
-						  }, {
-							  value: 1,
-							  label: 'Lens'
-						  }, {
-							  value: 2,
-							  label: 'T-Spacer'
-						  }
-					  ],
-					  }
-					}
-		};
-		return {_vue};
-	  },
-	  'el-checkbox Tab控制'(){
-		  /*
-		  情境需求:
-		  1.基本的輸入,是以 [備註記錄],[位置碼] 為主
-		  2.因為[備註記錄] 大多都重覆,為了增加建檔效率,所以,設 Keep 功能,
-			讓[備註記錄] 可以保留,直接輸入下筆[位置碼]
-		  3.總合 1,2 的需求,輸入操作的控制為以下模式
-		  A:第一次輸入
-			tab依序為[備註記錄][Keep][位置碼]
-	
-		  需求變更,此段己不必要,先保留
-		  */
-		  var _vue = {
+		'bts.form-control'() {
+			/*
+			在套用 form-control 之後 , element UI 的樣式會出現跑版的問題,
+			目前仍找不到比較好的解決方案
+			*/
+			var _vue = {
 			template: `
-			  <div>
-				<el-input ref="errCode" placeholder="请输入内容" size="mini" style="width:50rem;" tabindex="1">
-				  <template slot="prepend">
-					  備註記錄
-				  </template>
-				  <template slot="append">
-					  <el-checkbox v-model="isKeep" @@click="isKeep=!isKeep"> Keep</el-checkbox>
-				  </template>
-				</el-input>
-		  
-				<el-input placeholder="请输入内容" size="mini" @@keyup.enter.native="mapCode_input($event)" v-model="mapCode" style="width:30rem;" tabindex="3">
+						<div>
+						<div><input type="checkbox" v-model="isUse" />套用 form-control</div>
+						<el-select :class="[isUse?'form-control':'']"  v-model="sel">
+						<el-option v-for="item in list_box_type"
+								:key="item.value"
+								:label="item.label"
+								:value="item.value">
+						</el-option>
+					</el-select>
+						</div>`,
+						data(){
+						return {
+							isUse:true,
+							sel:0,
+							list_box_type: [
+							{
+								value: 0,
+								label: 'ZGB'
+							}, {
+								value: 1,
+								label: 'Lens'
+							}, {
+								value: 2,
+								label: 'T-Spacer'
+							}
+						],
+						}
+						}
+			};
+			return {_vue};
+		},
+		'el-checkbox Tab控制'(){
+			/*
+			情境需求:
+			1.基本的輸入,是以 [備註記錄],[位置碼] 為主
+			2.因為[備註記錄] 大多都重覆,為了增加建檔效率,所以,設 Keep 功能,
+				讓[備註記錄] 可以保留,直接輸入下筆[位置碼]
+			3.總合 1,2 的需求,輸入操作的控制為以下模式
+			A:第一次輸入
+				tab依序為[備註記錄][Keep][位置碼]
+		
+			需求變更,此段己不必要,先保留
+			*/
+			var _vue = {
+				template: `
+				<div>
+					<el-input ref="errCode" placeholder="请输入内容" size="mini" style="width:50rem;" tabindex="1">
 					<template slot="prepend">
-						位置碼
+						備註記錄
 					</template>
-				</el-input>
-			</div>`,
+					<template slot="append">
+						<el-checkbox v-model="isKeep" @@click="isKeep=!isKeep"> Keep</el-checkbox>
+					</template>
+					</el-input>
+			
+					<el-input placeholder="请输入内容" size="mini" @@keyup.enter.native="mapCode_input($event)" v-model="mapCode" style="width:30rem;" tabindex="3">
+						<template slot="prepend">
+							位置碼
+						</template>
+					</el-input>
+				</div>`,
+				data(){
+				return {
+					isKeep: false,
+					mapCode: "",
+					errCode: "",
+					
+				}
+				},
+				methods: {
+				isKeepErrCode(isErrCodeFocus) {
+					if (this.isKeep == false) {
+						this.errCode = ""
+						if (isErrCodeFocus) this.$refs.errCode.focus();
+					};
+				},
+				},
+			};
+			return {_vue};
+		},
+		'el-dialog 樣式'(){
+			/*
+			情境需求:
+			1.自定義 dialog header 樣式
+			2.dialog 設定圓角
+				2-1. el-dialog__header 需要再補設 border-radius ,
+					才不會造成頭部的圓角被蓋掉
+		
+			*/
+			var _css = `
+			.el-dialog__header {
+				border-radius: 7px 7px 0px 0px;
+				background-color: #5d9cec;
+				padding: 13px !important;
+				color: #fff;
+				text-align: left;
+			}
+		
+			.el-dialog {
+				border-radius: 7px;
+			}
+		
+			
+			`;
+			var _vue = {
+			template: `
+			<div>
+			<el-dialog  
+					:append-to-body="true"
+					:visible.sync="dialog1"
+					width="35rem"
+					class="ver-modify"
+					:show-close="false"
+					center>
+			<label slot="title">
+				版本管理
+			</label>
+		
+				<span slot="footer" class="dialog-footer">
+				<el-button @@click="dialog1=false">取 消</el-button>
+				<el-button type="primary" @@click="dialog1=false">确 定</el-button>
+				</span>
+			</el-dialog></div>`,
 			data(){
-			  return {
-				isKeep: false,
-				mapCode: "",
-				errCode: "",
-				
-			  }
+				return {
+				dialog1: true
+		
+				}
 			},
 			methods: {
-			  isKeepErrCode(isErrCodeFocus) {
-				  if (this.isKeep == false) {
-					  this.errCode = ""
-					  if (isErrCodeFocus) this.$refs.errCode.focus();
-				  };
-			  },
-			},
-		  };
-		  return {_vue};
-	  },
-	  'el-dialog 樣式'(){
-		/*
-		  情境需求:
-		  1.自定義 dialog header 樣式
-		  2.dialog 設定圓角
-			2-1. el-dialog__header 需要再補設 border-radius ,
-				才不會造成頭部的圓角被蓋掉
-	
-		  */
-		var _css = `
-		  .el-dialog__header {
-			border-radius: 7px 7px 0px 0px;
-			background-color: #5d9cec;
-			padding: 13px !important;
-			color: #fff;
-			text-align: left;
-		  }
-	
-		  .el-dialog {
-			  border-radius: 7px;
-		  }
-	
-		  
-		`;
-		var _vue = {
-		  template: `
-		  <div>
-		  <el-dialog  
-				:append-to-body="true"
-				:visible.sync="dialog1"
-				width="35rem"
-				class="ver-modify"
-				:show-close="false"
-				center>
-		  <label slot="title">
-			版本管理
-		  </label>
-	 
-			<span slot="footer" class="dialog-footer">
-			  <el-button @@click="dialog1=false">取 消</el-button>
-			  <el-button type="primary" @@click="dialog1=false">确 定</el-button>
-			</span>
-		  </el-dialog></div>`,
-		  data(){
-			return {
-			  dialog1: true
-	
-			}
-		  },
-		  methods: {
-	 
-		  },
-		};
-		return {_vue,_css};
-	  },
-	
-	  '版本控制'() {
-		var _obj = {
-		   _vue:{
-			  template: `
-			  <div>
-			  <el-autocomplete
-			  popper-class="my-autocomplete"
-			  v-model="state"
-			  :fetch-suggestions="querySearch"
-			  placeholder="请输入内容"
-			  @select="handleSelect">
-			  <template slot="append">
-			  <el-switch
-				active-color="#13ce66"
-				inactive-color="#ff4949">
-			  </el-switch>
-			  
-			  </template>
-			  <template slot="prepend">
-				<el-button type="primary" icon="el-icon-s-flag"></el-button>
-			  </template>
-			  
-			  <i
-				class="el-icon-search el-input__icon"
-				slot="suffix"
-				@click="handleIconClick">
-			  </i>
-			  <template slot-scope="{ item }">
-				<div class="name">{{ item.value }}</div>
-				<span class="addr">{{ item.address }}</span>
-			  </template>
-			  
-			</el-autocomplete>
 		
-			<el-autocomplete>
-			  <template slot="prepend">
-				<el-button type="primary" icon="el-icon-s-flag"></el-button>
-			  </template>
-			  <template>
-				<el-select >
-				<el-option
-					v-for="item in ver_list"
-					:key="item.ver"
-					:label="item.ver"
-					:value="item.ver">
-				  </el-option>
-				</el-select>
-			  </template>
-			  <template slot="append">
-				  <el-switch
+			},
+			};
+			return {_vue,_css};
+		},
+		
+		'版本控制'() {
+			var _obj = {
+			_vue:{
+				template: `
+				<div>
+				<el-autocomplete
+				popper-class="my-autocomplete"
+				v-model="state"
+				:fetch-suggestions="querySearch"
+				placeholder="请输入内容"
+				@select="handleSelect">
+				<template slot="append">
+				<el-switch
 					active-color="#13ce66"
 					inactive-color="#ff4949">
-				  </el-switch>
-			  </template>
-		  </el-autocomplete>
-	 </div>
+				</el-switch>
+				
+				</template>
+				<template slot="prepend">
+					<el-button type="primary" icon="el-icon-s-flag"></el-button>
+				</template>
+				
+				<i
+					class="el-icon-search el-input__icon"
+					slot="suffix"
+					@click="handleIconClick">
+				</i>
+				<template slot-scope="{ item }">
+					<div class="name">{{ item.value }}</div>
+					<span class="addr">{{ item.address }}</span>
+				</template>
+				
+				</el-autocomplete>
 			
-			  `,
-			  data() {
-				return {
-				  restaurants: [],
-				  state: '',
-				  ver_list:[
-					  { ver: 1, en: true ,def:false},
-					  { ver: 2, en: false ,def:false},
-					  {ver:3,en:false,def:true}
-				  ]
-				};
-			  },
-			  methods: {
-				querySearch(queryString, cb) {
-				  var restaurants = this.restaurants;
-				  var results = queryString ? restaurants.filter(this.createFilter(queryString)) : restaurants;
-				  // 调用 callback 返回建议列表的数据
-				  cb(results);
+				<el-autocomplete>
+				<template slot="prepend">
+					<el-button type="primary" icon="el-icon-s-flag"></el-button>
+				</template>
+				<template>
+					<el-select >
+					<el-option
+						v-for="item in ver_list"
+						:key="item.ver"
+						:label="item.ver"
+						:value="item.ver">
+					</el-option>
+					</el-select>
+				</template>
+				<template slot="append">
+					<el-switch
+						active-color="#13ce66"
+						inactive-color="#ff4949">
+					</el-switch>
+				</template>
+			</el-autocomplete>
+		</div>
+				
+				`,
+				data() {
+					return {
+					restaurants: [],
+					state: '',
+					ver_list:[
+						{ ver: 1, en: true ,def:false},
+						{ ver: 2, en: false ,def:false},
+						{ver:3,en:false,def:true}
+					]
+					};
 				},
-				createFilter(queryString) {
-				  return (restaurant) => {
-					return (restaurant.value.toLowerCase().indexOf(queryString.toLowerCase()) === 0);
-				  };
+				methods: {
+					querySearch(queryString, cb) {
+					var restaurants = this.restaurants;
+					var results = queryString ? restaurants.filter(this.createFilter(queryString)) : restaurants;
+					// 调用 callback 返回建议列表的数据
+					cb(results);
+					},
+					createFilter(queryString) {
+					return (restaurant) => {
+						return (restaurant.value.toLowerCase().indexOf(queryString.toLowerCase()) === 0);
+					};
+					},
+					loadAll() {
+					return [
+						{ "value": "三全鲜食（北新泾店）", "address": "长宁区新渔路144号" },
+						{ "value": "Hot honey 首尔炸鸡（仙霞路）", "address": "上海市长宁区淞虹路661号" },
+						{ "value": "新旺角茶餐厅", "address": "上海市普陀区真北路988号创邑金沙谷6号楼113" },
+						{ "value": "泷千家(天山西路店)", "address": "天山西路438号" },
+						{ "value": "胖仙女纸杯蛋糕（上海凌空店）", "address": "上海市长宁区金钟路968号1幢18号楼一层商铺18-101" },
+						{ "value": "贡茶", "address": "上海市长宁区金钟路633号" },
+						{ "value": "豪大大香鸡排超级奶爸", "address": "上海市嘉定区曹安公路曹安路1685号" },
+						{ "value": "茶芝兰（奶茶，手抓饼）", "address": "上海市普陀区同普路1435号" },
+						{ "value": "十二泷町", "address": "上海市北翟路1444弄81号B幢-107" },
+						{ "value": "星移浓缩咖啡", "address": "上海市嘉定区新郁路817号" },
+						{ "value": "阿姨奶茶/豪大大", "address": "嘉定区曹安路1611号" },
+						{ "value": "新麦甜四季甜品炸鸡", "address": "嘉定区曹安公路2383弄55号" },
+						{ "value": "Monica摩托主题咖啡店", "address": "嘉定区江桥镇曹安公路2409号1F，2383弄62号1F" },
+						{ "value": "浮生若茶（凌空soho店）", "address": "上海长宁区金钟路968号9号楼地下一层" },
+						{ "value": "NONO JUICE  鲜榨果汁", "address": "上海市长宁区天山西路119号" },
+						{ "value": "CoCo都可(北新泾店）", "address": "上海市长宁区仙霞西路" },
+						{ "value": "快乐柠檬（神州智慧店）", "address": "上海市长宁区天山西路567号1层R117号店铺" },
+						{ "value": "Merci Paul cafe", "address": "上海市普陀区光复西路丹巴路28弄6号楼819" },
+						{ "value": "猫山王（西郊百联店）", "address": "上海市长宁区仙霞西路88号第一层G05-F01-1-306" },
+						{ "value": "枪会山", "address": "上海市普陀区棕榈路" },
+						{ "value": "纵食", "address": "元丰天山花园(东门) 双流路267号" },
+						{ "value": "钱记", "address": "上海市长宁区天山西路" },
+						{ "value": "壹杯加", "address": "上海市长宁区通协路" },
+						{ "value": "唦哇嘀咖", "address": "上海市长宁区新泾镇金钟路999号2幢（B幢）第01层第1-02A单元" },
+						{ "value": "爱茜茜里(西郊百联)", "address": "长宁区仙霞西路88号1305室" },
+						{ "value": "爱茜茜里(近铁广场)", "address": "上海市普陀区真北路818号近铁城市广场北区地下二楼N-B2-O2-C商铺" },
+						{ "value": "鲜果榨汁（金沙江路和美广店）", "address": "普陀区金沙江路2239号金沙和美广场B1-10-6" },
+						{ "value": "开心丽果（缤谷店）", "address": "上海市长宁区威宁路天山路341号" },
+						{ "value": "超级鸡车（丰庄路店）", "address": "上海市嘉定区丰庄路240号" },
+						{ "value": "妙生活果园（北新泾店）", "address": "长宁区新渔路144号" },
+						{ "value": "香宜度麻辣香锅", "address": "长宁区淞虹路148号" },
+						{ "value": "凡仔汉堡（老真北路店）", "address": "上海市普陀区老真北路160号" },
+						{ "value": "港式小铺", "address": "上海市长宁区金钟路968号15楼15-105室" },
+						{ "value": "蜀香源麻辣香锅（剑河路店）", "address": "剑河路443-1" },
+						{ "value": "北京饺子馆", "address": "长宁区北新泾街道天山西路490-1号" },
+						{ "value": "饭典*新简餐（凌空SOHO店）", "address": "上海市长宁区金钟路968号9号楼地下一层9-83室" },
+						{ "value": "焦耳·川式快餐（金钟路店）", "address": "上海市金钟路633号地下一层甲部" },
+						{ "value": "动力鸡车", "address": "长宁区仙霞西路299弄3号101B" },
+						{ "value": "浏阳蒸菜", "address": "天山西路430号" },
+						{ "value": "四海游龙（天山西路店）", "address": "上海市长宁区天山西路" },
+						{ "value": "樱花食堂（凌空店）", "address": "上海市长宁区金钟路968号15楼15-105室" },
+						{ "value": "壹分米客家传统调制米粉(天山店)", "address": "天山西路428号" },
+						{ "value": "福荣祥烧腊（平溪路店）", "address": "上海市长宁区协和路福泉路255弄57-73号" },
+						{ "value": "速记黄焖鸡米饭", "address": "上海市长宁区北新泾街道金钟路180号1层01号摊位" },
+						{ "value": "红辣椒麻辣烫", "address": "上海市长宁区天山西路492号" },
+						{ "value": "(小杨生煎)西郊百联餐厅", "address": "长宁区仙霞西路88号百联2楼" },
+						{ "value": "阳阳麻辣烫", "address": "天山西路389号" },
+						{ "value": "南拳妈妈龙虾盖浇饭", "address": "普陀区金沙江路1699号鑫乐惠美食广场A13" }
+					];
+					},
+					handleSelect(item) {
+					console.log(item);
+					},
+					handleIconClick(ev) {
+					console.log(ev);
+					}
 				},
-				loadAll() {
-				  return [
-					{ "value": "三全鲜食（北新泾店）", "address": "长宁区新渔路144号" },
-					{ "value": "Hot honey 首尔炸鸡（仙霞路）", "address": "上海市长宁区淞虹路661号" },
-					{ "value": "新旺角茶餐厅", "address": "上海市普陀区真北路988号创邑金沙谷6号楼113" },
-					{ "value": "泷千家(天山西路店)", "address": "天山西路438号" },
-					{ "value": "胖仙女纸杯蛋糕（上海凌空店）", "address": "上海市长宁区金钟路968号1幢18号楼一层商铺18-101" },
-					{ "value": "贡茶", "address": "上海市长宁区金钟路633号" },
-					{ "value": "豪大大香鸡排超级奶爸", "address": "上海市嘉定区曹安公路曹安路1685号" },
-					{ "value": "茶芝兰（奶茶，手抓饼）", "address": "上海市普陀区同普路1435号" },
-					{ "value": "十二泷町", "address": "上海市北翟路1444弄81号B幢-107" },
-					{ "value": "星移浓缩咖啡", "address": "上海市嘉定区新郁路817号" },
-					{ "value": "阿姨奶茶/豪大大", "address": "嘉定区曹安路1611号" },
-					{ "value": "新麦甜四季甜品炸鸡", "address": "嘉定区曹安公路2383弄55号" },
-					{ "value": "Monica摩托主题咖啡店", "address": "嘉定区江桥镇曹安公路2409号1F，2383弄62号1F" },
-					{ "value": "浮生若茶（凌空soho店）", "address": "上海长宁区金钟路968号9号楼地下一层" },
-					{ "value": "NONO JUICE  鲜榨果汁", "address": "上海市长宁区天山西路119号" },
-					{ "value": "CoCo都可(北新泾店）", "address": "上海市长宁区仙霞西路" },
-					{ "value": "快乐柠檬（神州智慧店）", "address": "上海市长宁区天山西路567号1层R117号店铺" },
-					{ "value": "Merci Paul cafe", "address": "上海市普陀区光复西路丹巴路28弄6号楼819" },
-					{ "value": "猫山王（西郊百联店）", "address": "上海市长宁区仙霞西路88号第一层G05-F01-1-306" },
-					{ "value": "枪会山", "address": "上海市普陀区棕榈路" },
-					{ "value": "纵食", "address": "元丰天山花园(东门) 双流路267号" },
-					{ "value": "钱记", "address": "上海市长宁区天山西路" },
-					{ "value": "壹杯加", "address": "上海市长宁区通协路" },
-					{ "value": "唦哇嘀咖", "address": "上海市长宁区新泾镇金钟路999号2幢（B幢）第01层第1-02A单元" },
-					{ "value": "爱茜茜里(西郊百联)", "address": "长宁区仙霞西路88号1305室" },
-					{ "value": "爱茜茜里(近铁广场)", "address": "上海市普陀区真北路818号近铁城市广场北区地下二楼N-B2-O2-C商铺" },
-					{ "value": "鲜果榨汁（金沙江路和美广店）", "address": "普陀区金沙江路2239号金沙和美广场B1-10-6" },
-					{ "value": "开心丽果（缤谷店）", "address": "上海市长宁区威宁路天山路341号" },
-					{ "value": "超级鸡车（丰庄路店）", "address": "上海市嘉定区丰庄路240号" },
-					{ "value": "妙生活果园（北新泾店）", "address": "长宁区新渔路144号" },
-					{ "value": "香宜度麻辣香锅", "address": "长宁区淞虹路148号" },
-					{ "value": "凡仔汉堡（老真北路店）", "address": "上海市普陀区老真北路160号" },
-					{ "value": "港式小铺", "address": "上海市长宁区金钟路968号15楼15-105室" },
-					{ "value": "蜀香源麻辣香锅（剑河路店）", "address": "剑河路443-1" },
-					{ "value": "北京饺子馆", "address": "长宁区北新泾街道天山西路490-1号" },
-					{ "value": "饭典*新简餐（凌空SOHO店）", "address": "上海市长宁区金钟路968号9号楼地下一层9-83室" },
-					{ "value": "焦耳·川式快餐（金钟路店）", "address": "上海市金钟路633号地下一层甲部" },
-					{ "value": "动力鸡车", "address": "长宁区仙霞西路299弄3号101B" },
-					{ "value": "浏阳蒸菜", "address": "天山西路430号" },
-					{ "value": "四海游龙（天山西路店）", "address": "上海市长宁区天山西路" },
-					{ "value": "樱花食堂（凌空店）", "address": "上海市长宁区金钟路968号15楼15-105室" },
-					{ "value": "壹分米客家传统调制米粉(天山店)", "address": "天山西路428号" },
-					{ "value": "福荣祥烧腊（平溪路店）", "address": "上海市长宁区协和路福泉路255弄57-73号" },
-					{ "value": "速记黄焖鸡米饭", "address": "上海市长宁区北新泾街道金钟路180号1层01号摊位" },
-					{ "value": "红辣椒麻辣烫", "address": "上海市长宁区天山西路492号" },
-					{ "value": "(小杨生煎)西郊百联餐厅", "address": "长宁区仙霞西路88号百联2楼" },
-					{ "value": "阳阳麻辣烫", "address": "天山西路389号" },
-					{ "value": "南拳妈妈龙虾盖浇饭", "address": "普陀区金沙江路1699号鑫乐惠美食广场A13" }
-				  ];
-				},
-				handleSelect(item) {
-				  console.log(item);
-				},
-				handleIconClick(ev) {
-				  console.log(ev);
+				mounted() {
+					this.restaurants = this.loadAll();
 				}
-			  },
-			  mounted() {
-				this.restaurants = this.loadAll();
-			  }
-	
-		   }};
-		return _obj;
-	 },
+		
+			}};
+			return _obj;
+		},
+		'Reload-Timer'() {
+			var _note = `
+			<pre>
+			這個是專案上的需求 ,主要是要能呈現一個圓型計時圖示,以便做 Reload 的倒數計時,
+				並提供 提前 Reload (按鈕觸發)的功能
+			</pre>
+			`;
+			var _obj = {
+				_css:``,
+				_vue: {
+					template: `
+					<div>
+						${_note}
+						<el-button type="text" @click="renew" >
+							<el-progress type="circle" :percentage="percentage" 
+									width="20"
+									:show-text="false" :stroke-width="4"
+									color="#515253"></el-progress>
+						</el-button>
+					</div>
+					`,
+					data(){
+						return {
+							renewSec: 10,
+							times: 0,
+							timer: null,
+						}
+					},
+					mounted() {
+						this.fn_timer();
+					},
+					computed: {
+						percentage() {
+							var prgs = this.times / this.renewSec;
+							if (prgs >= 1) {
+								this.times = 0;
+								window.clearTimeout(this.timer);
+								return 0;
+							}
+							return prgs * 100;
+						}
+					},
+					methods: {
+						fn_timer(){
+							var _self = this;
+							_self.timer = setInterval(() => {
+								_self.times++;
+							}, 1000);							
+						},
+						renew(){
+							this.times = 0;
+							window.clearTimeout(this.timer);	
+						}
+					},
+				}
+			};
+			return _obj;
+		},
 	};
 	let Fail = {
 	
@@ -878,7 +936,7 @@ var __fn = (
 		*/
 	}
 	let Vue_Prd = {
-		'*pw-dyn-ui'() {
+		'pw-dyn-ui'() {
 			var _note = `
 			   <pre>
 			   </pre>
@@ -2064,7 +2122,7 @@ var __fn = (
 		},
 	}
 	var 程式產生器 = {
-		'*el-table-column'() {
+		'el-table-column'() {
 			var _note = `
 			   <pre>
 			   </pre>
