67a50dc7d638eef72a0c106628016295f42c6f53
anthony <a0982830615@gmail.com>
2020-11-16 11:37:26 +0800

    add

--- CSharp/Plugin/t_DynamicLinqQueryBuilder.cs
+++ CSharp/Plugin/t_DynamicLinqQueryBuilder.cs
@@ -125,5 +125,76 @@ namespace CSharp.Plugin
 
             }
         }
-	}
+	
+    
+        /// <summary>
+        /// 以下是工作上 碰到的實際 案例,需求情境如下
+        /// 1.利用 BuildQuery 完成查詢、排序處理
+        /// 2.因為 原始資料中,缺少了 REPORT ,所以需要再後補.
+        /// 而這個案例的重點就在下方註記中,處理 join 補資料的那一段 
+        /// </summary>
+        public void t_案例(){
+            /*
+            public static Result QueryWORKT(PagerQuery PQuery)
+            {
+                var _result = PQuery.chk();
+                if (_result.Success)
+                {
+                    using (var dbContext = new MDL.MESContext())
+                    {
+                        try
+                        {
+                            var _query = dbContext.ZZ_OPER_WORKT_SUMMARY.BuildQuery(PQuery.Conditions);
+
+                            if (PQuery?.Sort?.Code != "")
+                            {
+                                _query = _query.OrderBy(PQuery.Sort.Code);
+                            }
+                            if (PQuery.Page == null)
+                            {
+                                _result.Data = new { Queryable = _query.ToList() };
+                            }
+                            else
+                            {
+                                //先取得 要補資料的清單
+                                var Params = DDLServices.get_PARAMETERS("WorkingTimeType");
+                                var PageInfo = _query.PageResult(PQuery.Page.Index, PQuery.Page.Size);
+                                //需要先把 原始資料做 ToList()
+                                var _list = PageInfo.Queryable.ToList();
+                                //以下兩個清單,都需要做 AsQueryable() ,才能做處理
+                                var Queryable = (
+                                        from A0 in _list.AsQueryable()
+                                        join A1 in Params.AsQueryable()
+                                            on A0.REPORT_TYPE equals A1.PARAMETER_NO
+                                        select new
+                                        {
+                                            #region [ A0.* ] 
+                                            A0.SID,
+                                            ......
+                                            A0.UPDATE_USER,
+                                            A0.UPDATE_DATE,
+                                            #endregion
+                                            REPORT = A1.PARAMETER_NAME
+                                        }
+                                    )
+                                    .ToList();
+
+                                _result.Data = new { Queryable, PageInfo = PQuery.parsePagedResult(PageInfo) };
+
+                            }
+                        }
+                        catch (Exception Ex)
+                        {
+                            _result = new Result(ErrCode.GenericErrorMessage, ErrCode.False);
+                        }
+
+                    }
+                }
+                return _result;
+
+            }
+            
+            */
+        }
+    }
 }
