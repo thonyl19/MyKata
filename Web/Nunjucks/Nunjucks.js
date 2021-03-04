791d5ce286285a847462b865c96974fda27a3609
thonyl19 <a0982830615@gmail.com>
2021-03-04 16:49:01 +0800

    Add

--- Web/Nunjucks/Nunjucks.js
+++ Web/Nunjucks/Nunjucks.js
@@ -5,13 +5,13 @@ https://nunjucks.bootcss.com/
 import * as _ from 'lodash';
 import path from "path";
 const nunjucks = require('nunjucks');
-nunjucks.configure('QuokkaApp', { autoescape: true });
+nunjucks.configure('Nunjucks', { autoescape: true });
 const fs = require('fs');
 (()=>{
     var _tmp = {
         base:`<h1>Hello {{ name }}</h1>`,
-        array:`{% for ingredient, amount in food %}
-                    Use {{ amount }} of {{ ingredient }}
+        array:`{% for key, val in food %}
+                    Use [key]{{ key }} of {{ val }}
                 {% endfor %}`,
         array_解構:`{% for x, y, z in points %}
                 Point: {{ x }}, {{ y }}, {{ z }}
@@ -19,7 +19,7 @@ const fs = require('fs');
     }
     var fn = {
         save(data,filename="test.txt"){
-            fs.writeFileSync(`./QuokkaApp/${filename}`,data,'utf-8');
+            fs.writeFileSync(`./Nunjucks/${filename}`,data,'utf-8');
         },
         '讀入 template'(){
             var s = nunjucks.render('Nunjucks_t1.njk', { name: 'nunjucks' });
@@ -35,14 +35,14 @@ const fs = require('fs');
             var s = nunjucks.renderString(_tmp.base, { name: 'nunjucks' });
             s
         },
-        '解析陣列'(){
+        '解析物件'(){
             var food = {
                 'ketchup': '5 tbsp',
                 'mustard': '1 tbsp',
                 'pickle': '0 tbsp'
             };
             var s = nunjucks.renderString(_tmp.array,{food} );
-            s
+            console.log(s)
         },
         '陣列解構'(){
             var points = [[0, 1, 2], [5, 6, 7], [12, 13, 14]];
@@ -56,7 +56,7 @@ const fs = require('fs');
             var s = nunjucks.renderString(_tmp,{username:'apple'} );
             s
         },
-        '*block - 區塊運用'(){
+        'block - 區塊運用'(){
             /*
             1.在 Nunjucks_t1 設 block header
             2._tmp 中,繼承 Nunjucks_t1 , 並置換自己的 header
@@ -70,9 +70,33 @@ const fs = require('fs');
             s
         },
     } 
-    _.each(fn,(e,k)=>{
-        if (k.substr(0,1)=="*"){
-            e();
-        }
+    var _case = {
+        '*el_table'(){
+            var filed = {
+                "ROUTE_SID": "GTI20101517555209104",
+                "ROUTE_NO": "C030-19",
+                "ROUTE": "单面阳极板（子流程）",
+                "ROUTE_CATEGORY": "R",
+                "DEFAULT_VERSION": 1,
+                "VERSION_DESCRIPTION": null,
+                "MAX_VERSION": 2,
+                "CREATE_USER": "mes",
+                "CREATE_DATE": "2020-10-15 17:56:21",
+                "UPDATE_USER": "mes",
+                "UPDATE_DATE": "2020-10-30 14:13:41"
+            }
+            var s = nunjucks.render('el_table.njk',{filed} );
+            fn.save(s,"./_tmp.txt"); 
+        },
+
+    }
+
+
+    _.each([fn,_case],(_fn)=>{
+        _.each(_fn,(e,k)=>{
+            if (k.substr(0,1)=="*"){
+                e();
+            }
+        })
     })
 })()
