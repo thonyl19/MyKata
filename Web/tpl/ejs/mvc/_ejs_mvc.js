ab8db609fe1b17d01ace0837cd52d1b02bcc869e
thony <a0982830615@gmail.com>
2021-03-08 23:46:10 +0800

    Add

--- Web/tpl/ejs/mvc/_ejs_mvc.js
+++ Web/tpl/ejs/mvc/_ejs_mvc.js
@@ -34,28 +34,73 @@ ejs._ = _;
             { name: 'Jane', age: 6, species: 'ferret' }
           ]
     }
-    var _tpl = {
+    var _tpl_gti_table = {
         gti_el_table_col(filed){return `<el-table-column :label="i18n.${filed}"
     prop="${filed}" sortable="custom"
     :sort-orders="$UT.sort_order"></el-table-column>`},
-    
-
+        gti_el_table_操作欄位(filed){return `<el-table-column :label="i18n.操作" min-width="20">
+    <template slot-scope="scope">
+        <el-button size="small" circle 
+                    type="danger"
+                    icon="el-icon-delete"
+                    :disabled="scope.row.QUOTE_ONCE=='T'"
+                    @@click="e_del(scope.row.${filed})"
+                    ></el-button>
+    </template>
+</el-table-column>`},
+ 
+    }
+    var _tpl_gti_table_arg = {
+        gti_el_table_col(filed){return `<el-table-column :label="i18n.${filed.name}"
+    prop="${filed.name}" sortable="custom"
+    :sort-orders="$UT.sort_order">
+        <template slot-scope="scope">
+            <el-button type="text" @@click="act_Item(scope.row.${filed.name})">{{scope.row.${filed.name}}}</el-button>
+        </template>
+    </el-table-column>`},
+        gti_el_table_操作欄位(filed){return `<el-table-column :label="i18n.操作" min-width="20">
+    <template slot-scope="scope">
+        <el-button size="small" circle 
+                    type="danger"
+                    icon="el-icon-delete"
+                    :disabled="scope.row.QUOTE_ONCE=='T'"
+                    @@click="e_del(scope.row.${filed})"
+                    ></el-button>
+    </template>
+</el-table-column>`},
+ 
     }
  
  
  
     var _gti = {
-        async '*el_table'(){
+        async 'el_table'(){
+            var SID_Filed = 'ROUTE_SID'; 
             var arg = {
+                SID_Filed,
                 row:_data.filed_1,
+                Table_操作欄位:_tpl_gti_table.gti_el_table_操作欄位(SID_Filed).split('\n'),
                 TableColumn:[]
             }
             for(var filed in arg.row){
-                var arr = _tpl.gti_el_table_col(filed).split('\n');
+                var arr = _tpl_gti_table.gti_el_table_col(filed).split('\n');
                 arg.TableColumn = arg.TableColumn.concat(arr);
             }
             var s = await ejs.renderFile('./tpl/ejs/mvc/el_table.ejs',arg );
             ops.save(s,"~tmp.cshtml"); 
+        },
+        async 'el_table_arg'(){
+            var SID_Filed = 'ROUTE_SID'; 
+            var arg = {
+                SID_Filed,
+                row:_data.filed_1,
+            }
+            for(var filed in arg.row){
+                var arr = _tpl_gti_table.gti_el_table_col(filed).split('\n');
+                arg.TableColumn = arg.TableColumn.concat(arr);
+            }
+            var s = await ejs.renderFile('./tpl/ejs/mvc/el_table.ejs',arg );
+            ops.save(s,"~cfg.json"); 
         },
         async 'i18n_json'(){
             var arg = {
