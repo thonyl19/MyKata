f41c613f85ee854500db0f742366bea476dd70e4
thonyl19 <a0982830615@gmail.com>
2021-04-12 09:07:06 +0800

    Add

--- Web/PowerTool/api_app.js
+++ Web/PowerTool/api_app.js
@@ -6,7 +6,7 @@ const cors =require('@koa/cors');
 //const parameter = require('koa-parameter');
 const morgan =require('morgan'); 
  
-let ip = require('ip');
+//let ip = require('ip');
 var os = require( 'os' );
 
 //有空再試
@@ -148,11 +148,12 @@ Router.get('/api/user/:sid',async(ctx, next)=>{
 })
 */
 var networkInterfaces = os.networkInterfaces();
-var arr = networkInterfaces['Local Area Connection 3']
-var ip = arr[1].address
+var arr = networkInterfaces['Local Area Connection 3'];
+console.log(arr);
+var _ip = '127.0.0.1';//arr[1].address;
  
 console.log({mdb_demo1});
-console.log(`http://${ip}:3000/api`);
+console.log(`http://${_ip}:3000/api`);
 // enable CORS - Cross Origin Resource Sharing
 app.use(cors());
 //app.use(index);
