69530b1916bedb3ba9d40ed638efb7918157b537
thony <a0982830615@gmail.com>
2021-04-27 22:45:56 +0800

    add

---
+++ Web/tpl_ejsyaml/mvc_gti/vue_selectize/dynquery/_part.js
@@ -0,0 +1,26 @@
+﻿var part = {
+	parse(Src,_){
+		let {selectize} = Src;
+		let dynSet = {};
+		switch(selectize.valChgMode){
+			case 0://基本型
+				dynSet = {
+					v_model: `${selectize.name}.val`,
+				};
+				break;
+			case 1://Computed
+				dynSet = {
+					v_model: `c_${selectize.name}`,
+				};
+				break;
+			case 2://Watch
+				dynSet = {
+					v_model: `${selectize.name}.val`,
+				};
+				break;
+		}
+		$._.set(selectize,"dynSet",dynSet);
+		Src = {Src:selectize};
+		return Src;
+	}
+}
