fc26f9d6bf8ae67f0bf008e2d642b3ebe618f5b5
thony <a0982830615@gmail.com>
2021-05-05 23:34:18 +0800

    add

--- Web/tpl_ejsyaml/mvc_gti/_0_ejs.js
+++ Web/tpl_ejsyaml/mvc_gti/_0_ejs.js
@@ -1521,10 +1521,13 @@ const {ext_ut} = require('_test/ext_ut');
             })
 
         },
-        async '*inject'(){
+        async 'inject'(){
             var inject = {
                 path: 'D:\\A\\Code\\github\\MyKata\\MyKata_Web\\Web\\MVC\\gti\\',
                 file: 'SequenceNum.cshtml',
+            }
+            var _part = {
+                "Html_Code":"<el-radio-group v-model=\"form.ENABLE_FLAG\">\r\n\t<el-radio class=\"x\" label=\"Enable\">{{i18n.Enable}}</el-radio>\r\n\t<el-radio class=\"x\" label=\"Disable\">{{i18n.Disable}}</el-radio>\r\n\t<el-radio class=\"x\" label=\"AllStatus\">{{i18n.AllStatus}}</el-radio>\r\n</el-radio-group>",
             }
             var _target = `${inject.path}${inject.file}`;
             var _ejs = `${_target}.ejs`;
@@ -1546,6 +1549,58 @@ const {ext_ut} = require('_test/ext_ut');
                 _base = _base.replace(el,_Code);
             }) 
             ext_ut.writeFile(`${_target}`,_base);
+        },
+        parseInjectPoint(_match){
+            _match
+            let [point] = _match;
+            let [start] =  point.match(/(|\t)(.)+(##|#_)/g);
+            start 
+            let [tabs] = start.match(/(|\t)+/g); 
+            let [injectKey] = start.match(/(##|#_)/g); 
+            let arg = {
+                point,
+                tabs,
+                injectKey,
+                get injectAfter(){
+                    return this.injectKey == "##";
+                }
+            };
+            arg
+        },
+        async '*inject1'(){
+            var inject = {
+                path: 'D:\\A\\Code\\github\\MyKata\\MyKata_Web\\Web\\MVC\\gti\\',
+                file: 'SequenceNum.cshtml',
+            }
+            var _part = {
+                "Html_Code":"<el-radio-group v-model=\"form.ENABLE_FLAG\">\r\n\t<el-radio class=\"x\" label=\"Enable\">{{i18n.Enable}}</el-radio>\r\n\t<el-radio class=\"x\" label=\"Disable\">{{i18n.Disable}}</el-radio>\r\n\t<el-radio class=\"x\" label=\"AllStatus\">{{i18n.AllStatus}}</el-radio>\r\n</el-radio-group>",
+            }
+            var _target = `${inject.path}${inject.file}`;
+            var _ejs = `${_target}.ejs`;
+            let isReinject = fs.existsSync(_ejs);
+            var _src = isReinject ?_ejs : _target;
+            var _base = await fs.readFileSync(_src);
+            if (!isReinject) ext_ut.writeFile(`${_ejs}`,_base);
+            _base = _base.toString();
+            let x = _base.match(/(|\t)(.)+##(.)+/g); 
+            var Code = "<el-radio-group v-model=\"form.ENABLE_FLAG\">\r\n\t<el-radio class=\"x\" label=\"Enable\">{{i18n.Enable}}</el-radio>\r\n\t<el-radio class=\"x\" label=\"Disable\">{{i18n.Disable}}</el-radio>\r\n\t<el-radio class=\"x\" label=\"AllStatus\">{{i18n.AllStatus}}</el-radio>\r\n</el-radio-group>";
+            _.each(_part,(v,k)=>{
+                var _reg = new RegExp(`(|\t)(.)+(##|#_)${k}(.)+`,'gi');
+                var _match = _base.match(_reg);
+                _match
+                _test.parseInjectPoint(_match);  
+            })
+            x.map(el=>{
+                el
+                var tabs = el.split('\t').length -1;
+                tabs
+                var arr = Code.split('\r\n');
+                arr.unshift(el);
+                var _tab = '\t'.repeat(tabs);
+                var _Code = arr.join(`\n${_tab}`);
+                //_base = _base.replace(el,_Code);
+            }) 
+            //ext_ut.writeFile(`${_target}`,_base);
         }
     }
 
